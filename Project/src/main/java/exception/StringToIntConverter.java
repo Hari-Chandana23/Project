57cd5e1ac9031c924d9f5eac93e4a1e1c320645f
Hari-Chandana23 <harichandana2123@gmail.com>
2024-02-06 15:37:08 -0600

    exception

---
+++ Project/src/main/java/exception/StringToIntConverter.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class StringToIntConverter {
+    public int convertToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+            return -1; // Or any other default value indicating an error
+        }
+    }
+}
